8cc714f594abe467fa89f79d210183666f341110
Zach Chang <podeng0321@gmail.com>
2020-04-15 23:39:17 +0800

    signup ui

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,56 +1,75 @@
 import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import { SignupWrapper, IndexWrapper } from '../style';
 import signupHandler from '../handler/signupHandler';
+import { SignupWrapper, IndexWrapper } from '../style';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        background: '#a5a5a536'
+    },
+    textField: {
+        margin: '15px 0',
+        width: 300
+    },
+    confirmBtn: {
+        margin: '20px 0'
+    }
+}));
 
 export default signupHandler(props => {
     const { handleSubmit, handleInputChange, errors, isShowError } = props;
+    const classes = useStyles();
+
     return (
         <IndexWrapper>
             <SignupWrapper>
-                <Paper />
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        {isShowError && errors.username && <div>{errors.username}</div>}
-                        <input
-                            type="text"
-                            name="username"
-                            onChange={handleInputChange}
-                            className="form-control"
-                            placeholder="Username"
-                        
-                        />
-                    </div>
-                    <div>
-                        {isShowError && errors.email && <div>{errors.email}</div>}
-                        <input
-                            type="text"
-                            name="email"
-                            onChange={handleInputChange}
-                            placeholder="Email"
-                        />
-                    </div>
-                    <div>
-                        {isShowError && errors.password && <div>{errors.password}</div>}
-                        <input
-                            type="password"
-                            name="password"
-                            onChange={handleInputChange}
-                            className="form-control"
-                            placeholder="Password"
-                        />
-                    </div>
-                    <div>
-                        {isShowError && errors.confirmPassword && <div>{errors.confirmPassword}</div>}
-                        <input
-                            type="password"
-                            name="confirmPassword"
-                            onChange={handleInputChange}
-                            placeholder="Confirm Password"
-                        />
-                    </div>
-                    <button type="submit">Confirm</button>
-                </form>
+                <Paper className={classes.paper}>
+                    <form onSubmit={handleSubmit}>
+                        <TextField className={classes.textField} id="standard-basic" label="Username">
+                            {isShowError && errors.username && <div>{errors.username}</div>}
+                            <input
+                                type="text"
+                                name="username"
+                                onChange={handleInputChange}
+                                className="form-control"
+                                placeholder="Username"
+                            
+                            />
+                        </TextField>
+                        <TextField className={classes.textField} id="standard-basic" label="Email">
+                            {isShowError && errors.email && <div>{errors.email}</div>}
+                            <input
+                                type="text"
+                                name="email"
+                                onChange={handleInputChange}
+                                placeholder="Email"
+                            />
+                        </TextField>
+                        <TextField className={classes.textField} id="standard-basic" label="Password">
+                            {isShowError && errors.password && <div>{errors.password}</div>}
+                            <input
+                                type="password"
+                                name="password"
+                                onChange={handleInputChange}
+                                className="form-control"
+                                placeholder="Password"
+                            />
+                        </TextField>
+                        <TextField className={classes.textField} id="standard-basic" label="Confirm Password">
+                            {isShowError && errors.confirmPassword && <div>{errors.confirmPassword}</div>}
+                            <input
+                                type="password"
+                                name="confirmPassword"
+                                onChange={handleInputChange}
+                                placeholder="Confirm Password"
+                            />
+                        </TextField>
+                        <Button className={classes.confirmBtn} type='submit' variant="outlined">Confirm</Button>
+                    </form>
+                </Paper>
             </SignupWrapper>
         </IndexWrapper>
     )
--- src/style/SignupWrapper.js
+++ src/style/SignupWrapper.js
@@ -1,11 +1,17 @@
 import styled from 'styled-components';
 
 export const SignupWrapper = styled.div`
-    width: 100%;
+    width: 50%;
     text-align: center;
     position: absolute;
-    top: calc(50% - 60px);
-    height: 120px;
+    top: 20%;
+    left: 25%;
+
+    form {
+        width: 360px;
+        margin: 0 auto;
+        padding: 15px 0;
+    }
 `
 
 export const SignupItem = styled.div`
