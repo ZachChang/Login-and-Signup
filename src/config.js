203b04bcc6eac7f0c34648bd90cea7f0b1654130
Zach Chang <podeng0321@gmail.com>
2020-04-24 00:07:21 +0800

    config setting

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 module.exports = {
     apiRecord: apiRecord()
 }
-
 function apiRecord() {    
+    const isDev = process.env.NODE_ENV === 'development';
     return {
-        domain: process.env.REACT_APP_API_URL,
-        isDev: process.env.NODE_ENV === 'development'
+        domain: isDev ? 'http://localhost:8080' : 'http://localhost:8080/',
+        isDev
     }
 }
